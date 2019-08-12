22f8b570c02d12d7aa0be222005fc524c6b9ff66
Joseph Davidson <josephdavidson1989@gmail.com>
2019-08-12 16:31:15 +0300

    Updates

---
+++ precache-manifest.fd780227eb7a9b8b746218d6a2fd4100.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06e3ea88f4c15ca0f7ad7baddf49da9",
+    "url": "/friendsquiz/index.html"
+  },
+  {
+    "revision": "95f2fdd027511b7a48fc",
+    "url": "/friendsquiz/static/css/main.2472e5fa.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/friendsquiz/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "95f2fdd027511b7a48fc",
+    "url": "/friendsquiz/static/js/main.1d60befd.chunk.js"
+  },
+  {
+    "revision": "3762cf11801a2a523e25",
+    "url": "/friendsquiz/static/js/runtime~main.d22d7e9b.js"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/friendsquiz/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/friendsquiz/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "2473c908423cd91ba4f68fa11f57baa8",
+    "url": "/friendsquiz/static/media/correct.2473c908.gif"
+  },
+  {
+    "revision": "3e9964862b3ccd6ad9bfda5c9db243d2",
+    "url": "/friendsquiz/static/media/finish.3e996486.gif"
+  },
+  {
+    "revision": "0a969adf9fdfd775d04ac9d9c049cd94",
+    "url": "/friendsquiz/static/media/friendsFont.0a969adf.TTF"
+  },
+  {
+    "revision": "7290be31179e16c2fc24dec63a2b77c1",
+    "url": "/friendsquiz/static/media/incorrect.7290be31.gif"
+  }
+]);
